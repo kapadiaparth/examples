c22b276f0a364aedc8a43b13e7468e7368f4db12
David Z. Williams <dave@flex.io>
2018-02-28 01:15:11 -0500

    Moved Python code back inline in the pipe.

--- text-keywords/pipe.js
+++ text-keywords/pipe.js
@@ -1,10 +1,17 @@
+// uncomment the following line and insert your API key here to use the Flex.io JS SDK
+// Flexio.setup('YOUR_API_KEY')
 
-// This Python script uses BeautifulSoup to parse the HTML
-// of the specified website. The Python script interacts with the
-// Flex.io pipe via the `content.input.read()` and `content.output.write()`
-// methods. The Flex.io code handler `def flexio_handler(context):` is required.
-var py_parse_website_content = `
-
+// This is the Flex.io pipe which encapsulates all of the logic
+// required to read the HTML contents of a webpage, process it
+// and output a JSON array of items which can be consumed by D3.js
+var pipe = Flexio.pipe()
+  // request the content of the website URL that is specified in the HTML form
+  .request('${form.url}')
+  // this Python script uses BeautifulSoup to parse the HTML
+  // of the specified website. The Python script interacts with the
+  // Flex.io pipe via the `content.input.read()` and `content.output.write()`
+  // methods. The Flex.io code handler `def flexio_handler(context):` is required.
+  .python(`
 from bs4 import BeautifulSoup
 
 def flexio_handler(context):
@@ -19,14 +26,12 @@ def flexio_handler(context):
     text = '\n'.join(chunk for chunk in chunks if chunk)
     context.output.content_type = 'text/plain'
     context.output.write(text.encode('utf-8','ignore'))
-`
-
-// This Python script removes non-ascii characters and returns an array
-// of JSON objects with { id, value } keypairs. The Python script interacts with the
-// Flex.io pipe via the `content.input.read()` and `content.output.write()`
-// methods. The Flex.io code handler `def flexio_handler(context):` is required.
-var py_output_json_keypairs = `
-
+`)
+  // this Python script removes non-ascii characters and returns an array
+  // of JSON objects with { id, value } keypairs. The Python script interacts with the
+  // Flex.io pipe via the `content.input.read()` and `content.output.write()`
+  // methods. The Flex.io code handler `def flexio_handler(context):` is required.
+  .python(`
 import json
 from collections import defaultdict
 
@@ -52,25 +57,13 @@ def flexio_handler(context):
 
     context.output.content_type = "application/json"
     context.output.write(json.dumps(result))
-`
-
-// uncomment the following line and insert your API key here to use the Flex.io JS SDK
-// Flexio.setup('YOUR_API_KEY')
-
-// This is the Flex.io pipe which encapsulates all of the logic
-// required to read the HTML contents of a webpage, process it
-// and output a JSON array of items which can be consumed by D3.js
-var pipe = Flexio.pipe()
-  // request the content of the website URL that is specified in the HTML form
-  .request("${form.url}")
-  // use Python to read and parse the content of the HTML from the above website
-  .python(py_parse_website_content)
-  // use Python to convert the BeautifulSoup output into an array of JSON keypairs
-  .python(py_output_json_keypairs)
+`)
   // convert the JSON from the previous step into tabular format (which is necessary for filtering)
-  .convert("json", "table")
+  .convert('json', 'table')
   // filter the items based on the number of times they exist on the website
-  .filter("to_number(value) >= ${form.min_threshold} and to_number(value) <= ${form.max_threshold}")
+  .filter('to_number(value) >= ${form.min_threshold} and to_number(value) <= ${form.max_threshold}')
+  // run the pipe
+  .run()
 
 // You can save all of the logic of this pipe to your account in Flex.io. Saving a pipe is very useful
 // as it can now be referenced simply by calling the Flex.io REST API with the specified pipe alias.
