e41262395ee6f831b0d036ba51f84cd181dd9bff
Ken Kaczmarek <ken@flex.io>
2018-03-09 12:46:43 -0600

    updated comments

--- batch-csv-to-elasticsearch/pipe-twilio.js
+++ batch-csv-to-elasticsearch/pipe-twilio.js
@@ -5,15 +5,15 @@ var Flexio = require('flexio-sdk-js')
 // insert your API key here to use the Flex.io JS SDK with your account
 Flexio.setup('YOUR_API_KEY')
 
-// This is the Flex.io pipe which encapsulates all of the logic
-// required to read the CSV files and output them to Elasticsearch
+// This is the Flex.io pipe logic to read call log history from the Twilio API and writes it to Elasticsearch
+// Note that the aliases below will need to be replaced with your connection aliases (e.g. `{username}-twilio`)
 var pipe = Flexio.pipe()
   .read('/tutorial-twilio/calls')
   .convert('json', 'table')
   .write('tutorial-elasticsearch/call-log')
 
 // You may save this pipe to your Flex.io account, which enables a pipe endpoint to be called using an alias, via our
-// REST API or cURL. Note that the alias `examples-batch-copy-files-between-cloud-storage` below needs to be replaced
+// REST API or cURL. Note that the alias `examples-batch-csv-to-elasticsearch` below needs to be replaced
 // with your own alias in order to save this pipe to your account (e.g. `{username}-batch-csv-to-elasticsearch`)
 pipe.save({
   name: 'Copy CSV files to Elasticsearch',
