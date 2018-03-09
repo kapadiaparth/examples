9ecc6d5379b5fc185d43a7a2e0ac0e95ce9f3384
Ken Kaczmarek <ken@flex.io>
2018-03-09 11:33:49 -0600

    updated comments

--- batch-copy-files-between-cloud-storage/pipe.js
+++ batch-copy-files-between-cloud-storage/pipe.js
@@ -5,19 +5,15 @@ var Flexio = require('flexio-sdk-js')
 // insert your API key here to use the Flex.io JS SDK with your account
 Flexio.setup('YOUR_API_KEY')
 
-// TODO (replace with your text): This is the Flex.io pipe which encapsulates all of the logic
-// required to read the CSV files and output them to Elasticsearch
+// This is the Flex.io pipe logic to read a CSV file from AWS S3 and write it to Dropbox
+// Note that the aliases below will need to be replaced with your connection aliases (e.g. `{username}-s3`)
 var pipe = Flexio.pipe()
   .read('/tutorial-s3/contacts.csv')
   .write('/tutorial-dropbox/contacts.csv')
 
-// Flex.io pipes can contain quite a bit of logic and code -- this is one thing that differentiates Flex.io from
-// other serverless offerings. We can save all of the logic of this pipe to your account in Flex.io. Saving a pipe
-// is very useful as it will allow it to be called via the REST API or a cURL call with the specified pipe alias.
-
-// NOTE: The alias `examples-batch-copy-files-between-cloud-storage` below needs to be replaced with your own in order
-//       to save this pipe to your account. Best practices for aliases are to use your username
-//       as a prefix (e.g. `{username}-batch-copy-files-between-cloud-storage`)
+// You may save your pipe in the Flex.io app, which enables a pipe endpoint to be called using an alias, via REST
+// API or cURL. Note that the alias `examples-batch-copy-files-between-cloud-storage` below needs to be replaced with
+// your own alias in order to save this pipe to your account (e.g. `{username}-batch-copy-files-between-cloud-storage`).
 pipe.save({
   name: 'Copy Single File Between Cloud Storage',
   ename: 'examples-batch-copy-files-between-cloud-storage'
