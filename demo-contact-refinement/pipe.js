d9bb517437375a0a20b082fbd2603da0e13d762e
David Z. Williams <dave@flex.io>
2018-03-13 19:17:27 -0400

    Added pipe.js file for Contact Refinement example.

---
+++ demo-contact-refinement/pipe.js
@@ -0,0 +1,26 @@
+// if you haven't yet install the Flex.io JS SDK, you'll need to do that
+// before running this script. To do so, run `npm install flexio-sdk-js`.
+var Flexio = require('flexio-sdk-js')
+
+// insert your API key here to use the Flex.io JS SDK with your account
+Flexio.setup('YOUR_API_KEY')
+
+// This is the Flex.io pipe logic to read a CSV file from GitHub,
+// convert it to a table, select a subset of columns, transform all
+// text to uppercase and output the result in JSON.
+var pipe = Flexio.pipe()
+  .request("https://raw.githubusercontent.com/flexiodata/examples/master/demo-contact-refinement/source-data/contacts-ltd1.csv")
+  .convert("csv", "table")
+  .select("givenname","surname","streetaddress","city","state","zipcode")
+  .transform( { "columns": [ "givenname", "surname", "streetaddress", "city" ], "operations": [{ "operation": "case", "case": "upper" }] } )
+  .convert("table", "json")
+
+// You may save this pipe to your Flex.io account, which enables a pipe endpoint to be called using an alias, via our
+// REST API or cURL. Note that the alias `examples-demo-contact-refinement` below needs to be replaced
+// with your own alias in order to save this pipe to your account (e.g. `{username}-demo-contact-refinement`)
+pipe.save({
+  name: 'Contact Refinement',
+  ename: 'examples-demo-contact-refinement'
+})
+
+
